ed3dfc81502e355ef156cc4e72f0856032f76eb0
Nilesh <shree05sonawane@gmail.com>
2020-05-04 22:29:33 +0530

    Added day 4 problem and solution-1 for the same

---
+++ 4-NumberComplement.py
@@ -0,0 +1,52 @@
+#PROBLEM
+#Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+
+#Example 1:
+#Input: 5
+#Output: 2
+#Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+
+#Example 2:
+#Input: 1
+#Output: 0
+#Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+
+#Note:
+#The given integer is guaranteed to fit within the range of a 32-bit signed integer.
+#You could assume no leading zero bit in the integer’s binary representation.
+
+
+
+
+
+#SOLUTION-1
+class Solution:
+    def intToBinCompString(self, intVal: int) -> str:
+        """
+        Takes interger value as an input.
+        Returns Complement of binary value of the given interger 
+        """
+        if intVal == 0: 
+            return "1" 
+        strVal = "" 
+        while intVal > 0: 
+            if intVal%2 != 0: 
+                strVal = "0" + strVal 
+            else: 
+                strVal = "1" + strVal 
+            intVal = intVal//2 
+        return strVal 
+    def binaryToInt(self, binVal: str) -> int: 
+        """
+        Takes in binary representation as string.
+        Returns base-10 number of given binary
+        """
+        intVal = 0 
+        for i,val in enumerate(binVal[::-1]): 
+            if val == '1': 
+                intVal = intVal + (2**i) 
+        return intVal 
+    def findComplement(self, num: int) -> int:
+        return self.binaryToInt(self.intToBinCompString(num))
+
+
