b1b4be36a438a13cb0bea7bfc7817e2546593793
Nilesh <shree05sonawane@gmail.com>
2020-05-20 22:53:01 +0530

    Add day 20 code

---
+++ 20-KthSmallestElementInBST.py
@@ -0,0 +1,80 @@
+#PROBLEM
+# Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+# Note:
+# You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+# Example 1:
+
+# Input: root = [3,1,4,null,2], k = 1
+#    3
+#   / \
+#  1   4
+#   \
+#    2
+# Output: 1
+# Example 2:
+
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+#        5
+#       / \
+#      3   6
+#     / \
+#    2   4
+#   /
+#  1
+# Output: 3
+# Follow up:
+# What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+#SOLUTION-1
+class Solution:
+    def treeToList(self,root,li=[]):
+        if root == None:
+            return
+        li.append(root.val)
+        self.treeToList(root.left, li)
+        self.treeToList(root.right, li)
+        return li
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        arr = sorted(self.treeToList(root))
+        return arr[k-1]
+        
+
+
+#SOLUTION-2
+class Solution:
+    def treeToList(self,root,li=[]):
+        def inorder(r):
+            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+    
+        return inorder(root)[k - 1]
+
+
+
+#SOLUTION-3
+class Solution:
+    def treeToList(self,root,li=[]):
+        s, cur, rank = [], root, 0
+
+        while s or cur:
+            if cur:
+                s.append(cur)
+                cur = cur.left
+            else:
+                cur = s.pop()
+                rank += 1
+                if rank == k:
+                    return cur.val
+                cur = cur.right
+
+        return rank
